2f4bfa483945f1a566237c486582ec36c66a16e1
Christian Cabrera <cabrera.ch27@gmail.com>
2026-07-24 11:11:33 -0300

    Ajuste en la conexión a la base de datos

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -8,16 +8,21 @@ const { Pool } = pg;
 // Verificamos si estamos en Render (o producción) o en local
 const isProduction = process.env.NODE_ENV === "production";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
-  // Solo aplicamos SSL si estamos en producción (Render)
-  ssl: isProduction ? { rejectUnauthorized: false } : false
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: isProduction
+          ? { rejectUnauthorized: false }
+          : false,
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }
+);
 
 export default pool;
