3af7d56b334f08a551a38fcd18b8bf6942bdb571
ema789 <emasanchez473@outlook.com>
2026-08-03 20:54:45 -0300

    FIX ACTUALIZAR 1

--- frontend/src/app/register/page.jsx
+++ frontend/src/app/register/page.jsx
@@ -1,12 +1,9 @@
 import { RegisterForm } from "@/components/auth/RegisterForm";
 
-
 export default function RegisterPage() {
   return (
     <>
-      <main className="min-h-screen flex items-center justify-center bg-white px-5">
-        <RegisterForm />
-      </main>
+      <RegisterForm />
     </>
   );
 }
--- frontend/src/components/auth/LoginForm.jsx
+++ frontend/src/components/auth/LoginForm.jsx
@@ -38,7 +38,7 @@ export const LoginForm = () => {
   };
 
   return (
-    <div >
+    <div className="min-h-[calc(100vh-5rem)] flex flex-col justify-center mt-[-1rem] mb-[-6rem] pb-8">
 
       {isSubmitting && (
         <Loading variant="overlay" text="Iniciando sesión..." />
