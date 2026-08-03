721e12a167d2cd1f2f1f5741a4c10e70278b69c0
ema789 <emasanchez473@outlook.com>
2026-08-03 20:50:53 -0300

    FIX ACTUALIZAR

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -2,9 +2,9 @@ import { LoginForm } from "@/components/auth/LoginForm";
 export default function LoginPage() {
   return (
 
-      <main className="min-h-screen flex items-center justify-center bg-white px-5">
+      
         <LoginForm />
-      </main>
+      
    
   );
 }
