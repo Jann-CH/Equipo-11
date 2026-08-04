d5535203d1e6ea0e80015599fe3d948e5efa1039
ema789 <emasanchez473@outlook.com>
2026-08-03 21:00:13 -0300

    FIX ACTUALIZAR 3

--- frontend/src/components/auth/RegisterForm.jsx
+++ frontend/src/components/auth/RegisterForm.jsx
@@ -92,7 +92,7 @@ export const RegisterForm = () => {
 
   return (
 
-    <div className="w-full max-w-md py-6">
+    <div className="min-h-[calc(100vh-5rem)] flex flex-col justify-center mt-[-1rem] mb-[-6rem] pb-8">
 
       {isSubmitting && (
         <Loading variant="overlay" text="Iniciando sesión..." />
